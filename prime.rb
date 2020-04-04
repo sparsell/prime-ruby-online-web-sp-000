65add5b05e346738b1c4a1777871e7a8a56c3c83
Sonja Parsell <hello@sparsell.com>
2020-04-04 15:06:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1 || number == 0
+    false
+  else
+     number_range = (2..number)
+  number_range.each {|num| number % num == 0}
+      return true
+    end
+  end

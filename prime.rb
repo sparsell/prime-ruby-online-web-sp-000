40f5df86b418d8b6adf36e0b411fd36be68a3ed0
Sonja Parsell <hello@sparsell.com>
2020-04-04 15:08:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+  def prime?(number)
+  number_range = (2..number-1)
+  if number <= 1 || number == 0
+    false
+  elsif number == 2
+    true
+  else
+  number_range.each do |num|
+    return false if number % num == 0
+  end
+    true
+  end
+  end
